16f7a8f371605bbed05d052cae83e3f452a8950b
Patty Tituaña <t.cordova161@gmail.com>
2020-11-24 23:37:45 -0500

    probando nueva rama

--- src/app/productos/productos.page.ts
+++ src/app/productos/productos.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import {AgregarProductoPage} from '../agregar-producto/agregar-producto.page';
 
 @Component({
   selector: 'app-productos',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productos.page.scss'],
 })
 export class ProductosPage implements OnInit {
-
-  constructor() { }
+  productos: any=[{ titulo:"titulo2",texto:"texto"}]; 
+  constructor(public navCtrl:NavController, public modalController:ModalController) { }
 
   ngOnInit() {
   }
+  getItems($event){
+    const valor = $event.target.value;
+    console.log("valor del control"+valor)
 
+  }
+  agregarProducto(){
+    //this.navCtrl.push(agregarPage);
+   // var modal= this.modalController.create();
+   console.log("operacoin agregar")
+
+  }
+  editar(item){
+    console.log("operacoin editar")
+  }
+  eliminar(item){
+    console.log("operacoin eliminar")
+
+  }
 }
